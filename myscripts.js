3501442457fa64290a0ee49778e001308de4124e
speen-mp4 <77292409+speen-mp4@users.noreply.github.com>
2021-04-26 12:21:21 -0500

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -1,1 +1,21 @@
+document.getElementById("addBtn"). addEventListener("click", addText);
 
+function addText(){
+  var listItem = document.createElement("li");
+  listItem.innerText = document.getElementById("myInput").value;
+  document.getElementById("myUL").appendChild(listItem);
+  
+  //delete button
+  var deleteButton = document.createElement("button");
+  deleteButton.className  = "delete";
+  deleteButton.onclick=removeText;
+  deleteButton.innerText = "x";
+  listItem.appendChild(deleteButton);
+  }
+
+  function removeText(){  
+    var listItem=this.parentNode;
+    var ul=listItem.parentNode;
+    ul.removeChild(listItem);
+    
+  }
